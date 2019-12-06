2029da1fa2760fcf4ffc80cf4de95825dab1a0ae
Lars <lars.klippel@stud.th-bingen.de>
2019-12-06 16:23:24 +0100

    Spring Boot Initialization

---
+++ src/test/java/com/asys1920/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asys1920.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
