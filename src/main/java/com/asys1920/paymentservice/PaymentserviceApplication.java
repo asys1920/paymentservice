5143ed7db33177ba7a79c2a19873813345849937
Thomas <thomas.hormes@hotmail.de>
2020-02-27 14:33:43 +0100

    Added Graceful Shutdown

--- src/main/java/com/asys1920/paymentservice/PaymentserviceApplication.java
+++ src/main/java/com/asys1920/paymentservice/PaymentserviceApplication.java
@@ -3,6 +3,11 @@ package com.asys1920.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+import com.asys1920.util.GracefulShutdown;
 
 @EntityScan("com.asys1920.*")
 @SpringBootApplication
@@ -12,4 +17,16 @@ public class PaymentserviceApplication {
 		SpringApplication.run(PaymentserviceApplication.class, args);
 	}
 
+
+	@Bean
+	public GracefulShutdown gracefulShutdown() {
+		return new GracefulShutdown();
+	}
+
+	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdown gracefulShutdown) {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers(gracefulShutdown);
+		return factory;
+	}
 }
